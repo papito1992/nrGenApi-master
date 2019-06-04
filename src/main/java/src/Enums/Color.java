71cb44dbd3b246f928c6e45412351801524860bb
Martynas <narijauskasmartynas@gmail.com>
2019-06-04 19:27:01 +0300

    Enums + gitignore

---
+++ src/main/java/src/Enums/Color.java
@@ -0,0 +1,12 @@
+package src.Enums;
+
+public enum Color {
+    pink,
+    blue,
+    orange,
+    white,
+    green,
+    yellow,
+    black,
+    red
+}
---
+++ src/main/java/src/Enums/ColorToClaim.java
@@ -0,0 +1,12 @@
+package src.Enums;
+
+public enum ColorToClaim {
+    pink,
+    blue,
+    orange,
+    white,
+    green,
+    yellow,
+    black,
+    red
+}
